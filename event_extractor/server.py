8d20c8f0ad9f520ce83834efd51aba9989c31b2e
Chiao-Wei Hsu <cwhsu@iis.sinica.edu.tw>
2020-10-22 04:19:50 +0000

    Add server.py for single-doc usage

---
+++ event_extractor/server.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+from flask import Flask, abort, request
+import json
+import io
+from pathlib import Path
+from .main import EventExtractor
+from .event import Event
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello, this is an English Event Extractor API! Please access the url with <ip:port>/event?text=<text>\n"
+
+@app.route('/event', methods=['GET'])
+def get_event():
+
+    
+    text = request.args.get('text')
+    
+    temp_dir = Path('temp')
+    temp_dir.mkdir(exist_ok=True)
+    
+    temp_id = 99999
+    
+    with (temp_dir/str(temp_id)).open(mode='w') as f:
+        f.write(text)
+        
+    doc_id_dict = {temp_id: 'temp'}
+
+    events = EventExtractor.extract(doc_id_dict)
+    events_dict = Event.to_multidoc_dict(events)
+    result = {
+        'corefs': {},
+        'events': events_dict
+    }
+    result = {'text': text, 'result': result}
+
+    return json.dumps(result, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
